5ee0cad739dfe58713c659178a220a2fe0095e0a
Simonedambra <simonedambra130102@gmail.com>
2024-05-29 15:35:29 +0200

    first commit

---
+++ vite-hello-world/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import headercomponets from './components/headercomponent.vue';
+export default {
+  data() {
+  name:'myapp',}
+  return {
+
+
+  }
+  components:{
+    headercomponets,
+  }
+};
+</script>
+<template>
+<headercomponets \>
+</template>
+<style></style>
---
+++ vite-hello-world/src/components/headercomponent.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    data() {
+        name: 'header',
+    }
+        return {
+            intestazione: 'sono il titolo importato',
+        }
+    };
+
+
+</script>
+<template>
+    <h1>{{ intestazione }}</h1>
+</template>
+<style></style>
